0dac0fc208e0829aa8ca7a3a9a01f27daa905283
MyMac <ben.marcantel@gmail.com>
2019-01-24 13:00:38 -0600

    added speech refactored

--- space_corgi/yo.js
+++ space_corgi/yo.js
@@ -11,8 +11,34 @@ let image;
 let orbit;
 let orbitDir;
 let loader;
+let mouse;
+let raycaster;
+let synth;
+let utterThis;
 
 
+const talky = ()=>{
+    synth = window.speechSynthesis;
+    utterThis = new SpeechSynthesisUtterance("Oh, Hey, What is up dog?");
+    utterThis.rate = 0.6;
+    utterThis.pitch = 0;
+    synth.speak(utterThis);
+}
+
+const onclick = (event)=> {
+    talky();
+    // mouse = new THREE.Vector2();
+    // raycaster.setFromCamera(mouse, camera);
+    
+    // let intersects = raycaster.intersectObjects([mesh], true); //array
+    // if (intersects.length > 0) {
+    //     selectedObject = intersects[0];
+    //     console.log("selected",selectedObject)
+    //     if(mesh.name=="yo"){
+    //         talky();
+    //     }
+    // }
+}
 
 const init=()=>{
     image = THREE.ImageUtils.loadTexture('./images/pink.jpg');
@@ -24,26 +50,32 @@ const init=()=>{
     material2 = new THREE.MeshBasicMaterial({map: image2});
 
 
-    mesh = new THREE.Mesh(geometry.clone(),material.clone())
-    mesh2 = new THREE.Mesh(geometry.clone(),material2.clone())
+    mesh = new THREE.Mesh(geometry.clone(),material.clone());
+    mesh2 = new THREE.Mesh(geometry.clone(),material2.clone());
 
     mesh2.scale.x=.25;
     mesh2.scale.y=.25;
     mesh2.scale.z=.25;
     mesh2.position.x=300;
 
-    loader = new THREE.TextureLoader();
-    bgTexture = loader.load("./images/space.jpg");
-    scene.background = bgTexture;
+
+   
+    // loader = new THREE.TextureLoader();
+    // bgTexture = loader.load("./images/space.jpg");
+    // scene.background = bgTexture;
     
 
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({antialias: true,alpha:true});
     renderer.setSize(window.innerWidth,window.innerHeight);
-    document.body.appendChild(renderer.domElement)
+    document.body.appendChild(renderer.domElement);
+
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
+
+    renderer.domElement.addEventListener("click", onclick, true);
+    var selectedObject;
     
     orbit = new THREE.Group();
-   
-
     orbitDir = new THREE.Group();
     orbitDir.rotation.x = 0.25;
    
@@ -55,20 +87,18 @@ const init=()=>{
     orbit.add(mesh2);
     orbitDir.add(orbit);
     scene.add(orbitDir);
-    scene.add(camera)
+    scene.add(camera);
 }
 
-function onWindowResize(){
-
+const onWindowResize = ()=>{
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
-const animate = ()=> {
-    requestAnimationFrame(animate);
-    let date = Date.now() * .0001
+
+
+const render=()=>{
+    let date = Date.now() * .0001;
     mesh.rotation.x = date;
     mesh.rotation.y = date;
     mesh2.rotation.x = date * 2;
@@ -77,10 +107,14 @@ const animate = ()=> {
     renderer.autoClear = false;
     renderer.clear();
     renderer.render(scene, camera);
-    window.addEventListener( 'resize', onWindowResize, false );
-
-
 }
+
+const animate = ()=> {
+    requestAnimationFrame(animate);
+    render();   
+    window.addEventListener( 'resize', onWindowResize, false );
+}
+
 
 init();
 animate();
